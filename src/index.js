99e62f7a364b3a44cf2c76c844320802dd7725a8
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-03-28 14:19:42 -0600

    refactor file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import LoginForm from './LoginForm';
-import './index.css';
+import LoginForm from './components/LoginForm';
 
 ReactDOM.render(
   <LoginForm />,
