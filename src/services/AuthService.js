596efbb65de5b4b9ae26ecfbfad7573e76477406
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-04-03 13:47:09 -0600

    setup axios

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,7 +1,21 @@
-class AuthService {
+import axios from 'axios';
 
-  authenticate() {
-    console.log("stuff")
+class AuthService {
+  constructor() {
+    this.api = 'http://localhost:3000'
+  }
+
+  authenticate(email, password) {
+    axios.post(this.api, {
+      email: email,
+      password: password
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+  });
   }
 }
 
