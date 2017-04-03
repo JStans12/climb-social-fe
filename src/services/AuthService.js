fad9f574f01cdca53068fde0e3def1ba54085887
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-04-03 14:32:55 -0600

    save session expiration

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -12,8 +12,10 @@ class AuthService {
     })
     .then(function(response) {
       let tokens = response.data["tokens"];
+      let sessionExpiration = new Date(new Date().getTime() + (6.9 * 24 * 60 * 60 * 1000));
       sessionStorage.setItem('auth_token', tokens.auth_token);
       sessionStorage.setItem('refresh_token', tokens.refresh_token);
+      sessionStorage.setItem('sessionExpiration', sessionExpiration);
     })
     .catch(function(error) {
       console.log(error);
